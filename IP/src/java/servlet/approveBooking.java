ab971f6b7b8db69d99a7bde8f6555cde5a3a9371
ikmalfi3 <32659024+ikmalfi3@users.noreply.github.com>
2017-11-28 20:56:02 +0800

    ayam

--- IP/src/java/servlet/approveBooking.java
+++ IP/src/java/servlet/approveBooking.java
@@ -104,6 +104,7 @@ public class approveBooking extends HttpServlet {
             out.print(bookID +"<br>");
         } catch (Exception e) {
             e.printStackTrace();
+            
         }
     }
 
