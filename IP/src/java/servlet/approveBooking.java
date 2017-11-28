63822ac546f36d4e2d821463fdb0ec8eb6d35e80
ikmalfi3 <32659024+ikmalfi3@users.noreply.github.com>
2017-11-28 21:34:26 +0800

    ayam

--- IP/src/java/servlet/approveBooking.java
+++ IP/src/java/servlet/approveBooking.java
@@ -35,8 +35,7 @@ public class approveBooking extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        
+        PrintWriter out = response.getWriter(); 
         String homestay = request.getParameter("homestay");
         Date bookStart = null;
         Date bookEnd = null;
